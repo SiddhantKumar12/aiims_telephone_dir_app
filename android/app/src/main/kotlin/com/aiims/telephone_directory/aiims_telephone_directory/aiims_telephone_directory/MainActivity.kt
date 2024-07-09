0c091fa9c719335a98d1dff3002546454f8d1a5d
USER <siddhant0123456@gmail.com>
2024-07-09 10:51:33 +0530

    project aiims telephone directory

---
+++ android/app/src/main/kotlin/com/aiims/telephone_directory/aiims_telephone_directory/aiims_telephone_directory/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aiims.telephone_directory.aiims_telephone_directory.aiims_telephone_directory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
